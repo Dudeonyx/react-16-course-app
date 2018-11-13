2f1ebe0ccc27978d312043b95280ce84a850465f
Umeayo Onyekachukwu <umeayo.o.h@gmail.com>
2018-11-13 03:00:52 +0100

    Updates

---
+++ precache-manifest.428f8eb19c839d0666b3cdbe0413e9bf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0cbe2c28c09121172e08",
+    "url": "/react-16-course-app/static/js/runtime~main.0cbe2c28.js"
+  },
+  {
+    "revision": "61a37e0de95bb4ddfc25",
+    "url": "/react-16-course-app/static/js/main.61a37e0d.chunk.js"
+  },
+  {
+    "revision": "baa747e9cc9f5838e617",
+    "url": "/react-16-course-app/static/js/1.baa747e9.chunk.js"
+  },
+  {
+    "revision": "61a37e0de95bb4ddfc25",
+    "url": "/react-16-course-app/static/css/main.88f41e73.chunk.css"
+  },
+  {
+    "revision": "db99a40c340c9e7801b505bdcb31ce62",
+    "url": "/react-16-course-app/index.html"
+  }
+];
